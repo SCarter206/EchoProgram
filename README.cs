5b2458b373c75945e974330b2f7c638b41ed0202
Sean Carter <seans206email@gmail.com>
2019-10-14 03:05:03 -0700

    Update and rename README.md to README.cs

---
+++ README.cs
@@ -0,0 +1,17 @@
+//EchoProgram
+using System;
+
+namespace echo_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 echo = new Class1();
+            echo.Echo();
+        }
+    }
+}
+
+
+
